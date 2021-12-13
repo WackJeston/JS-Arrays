81308d3ce8f370b6da17f85fa43bbc7baa0b7e74
WackJeston <33kcaj33@gmail.com>
2021-12-13 11:17:45 +0000

    Completed group amends

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
 let imageZone = document.querySelector("#images")
 const image = document.querySelector("#img")
+let go = true
 let onLoad = true
 let firstTime = true
 let counter1 = 0
@@ -13,33 +14,38 @@ const emails = []
 
 // Using the data that the Axios.JS file grabbed from Picsum, this function creates usable varibales and displays the images on the page.
 function getRandomImage(data){
-  let randomImage = Math.floor(Math.random() * (data.length))
-  let displayImage = data[randomImage]
-  imageUrl = displayImage.download_url
-  imageAuthor = displayImage.author
-  // Here the main image is set and updated.
-  image.style.backgroundImage = `url("${imageUrl}")`
-  // This section uses JQuery to create my list of objects and their contents.
-  if (onLoad == false){
-    let email = document.querySelector("#email").value
-    let emailName0 = email.replace(".", "")
-    let emailName = emailName0.replace("@", "")
-    if (emails.includes(email) == false){
-      zoneGrow()
-      emails.push(email)
-      $("#images").prepend($("<div class='extra-layer'><div class='email-section-container " + emailName + "'><h2>" + email + "</h2><div class='list-item-img'><div class='list-item-info'><p><i class='fas fa-camera'></i> : " + oldImageAuthor + "</p></div></div></div></div>"))
-      $("." + emailName + " .list-item-img").first().css("background-image", 'url(' + oldImageUrl + ')')
-    } else {
-      $("#images ." + emailName).prepend($("<div class='list-item-img following-imgs'><div class='list-item-info'><p><i class='fas fa-camera'></i> : " + oldImageAuthor + "</p></div></div>"))
-      $("." + emailName + " .list-item-img").first().css("background-image", 'url(' + oldImageUrl + ')')
+  if (go == true) {
+    go = false
+    let randomImage = Math.floor(Math.random() * (data.length))
+    let displayImage = data[randomImage]
+    imageUrl = displayImage.download_url
+    imageAuthor = displayImage.author
+    // Here the main image is set and updated.
+    image.style.backgroundImage = `url("${imageUrl}")`
+    // This section uses JQuery to create the lists of objects and their contents.
+    if (onLoad == false){
+      let email = document.querySelector("#email").value
+      let emailName0 = email.replaceAll(".", "")
+      let emailName = emailName0.replace("@", "")
+      if (emails.includes(email) == false){
+        zoneGrow()
+        emails.push(email)
+        $("#images").prepend($("<div class='extra-layer'><div class='email-section-container " + emailName + "'><h2>" + email + "</h2><div class='list-item-img'><div class='list-item-info'><p><i class='fas fa-camera'></i> : " + oldImageAuthor + "</p></div></div></div></div>"))
+        $("." + emailName + " .list-item-img").first().css("background-image", 'url(' + oldImageUrl + ')')
+      } else {
+        $("#images ." + emailName).prepend($("<div class='list-item-img following-imgs'><div class='list-item-info'><p><i class='fas fa-camera'></i> : " + oldImageAuthor + "</p></div></div>"))
+        $("." + emailName + " .list-item-img").first().css("background-image", 'url(' + oldImageUrl + ')')
+      }
     }
-    // $("#images").append($("<div><div class='list-item-img'></div><div class='list-item-info'><p>" + email + "</p><p><i class='fas fa-camera'></i> : " + oldImageAuthor + "</p></div></div>"))
-    // $(".list-item-img").last().css("background-image", 'url(' + oldImageUrl + ')')
+    // Here the variables are updated to be ready for the next cycle of this function.
+    oldImageUrl = imageUrl
+    oldImageAuthor = imageAuthor
+    onLoad = false
+    setTimeout(function(){
+      go = true
+    }, 1000)
   }
-  // Here the variables are updated to be ready for the next cycle of this function.
-  oldImageUrl = imageUrl
-  oldImageAuthor = imageAuthor
-  onLoad = false
+
 }
 
 // This function keeps track of how many rows their should be in the list section.
--- js/submit.js
+++ js/submit.js
@@ -1,4 +1,4 @@
-const emailValidator = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailValidator = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const submitElement = document.querySelector("#submit")
 const emailElement = document.querySelector("#email")
 const formElement = document.querySelector("#form")
